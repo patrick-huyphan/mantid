b9c80981096ba444fc5dfb2291777069cdf5f5d0
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2018-01-05 15:10:17 -0500

    Re #123 Wavelength min and max calculated based on the input ws

--- Framework/Algorithms/inc/MantidAlgorithms/Q1DWeighted.h
+++ Framework/Algorithms/inc/MantidAlgorithms/Q1DWeighted.h
@@ -64,8 +64,6 @@ private:
   void init() override;
   /// Execution code
   void exec() override;
-
-  void setWavelengthBoundaries(Mantid::API::MatrixWorkspace_sptr ws);
 };
 
 } // namespace Algorithms
